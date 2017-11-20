735a92b8144b85055a53453ca42939e8d89bf7c8
YW <wangyouhao@YWde-MBP.hsd1.nj.comcast.net>
2017-11-20 15:49:44 -0500

    tried variables

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,9 +1,23 @@
 //console.log('Hello World!');
+
+
+
+// Lecture: variables
+//var name = 'John';
+//console.log(name);
+//var lastName = 'Smith';
+//console.log(lastName);
+//var age = 26
+//console.log(age);
+//var fullAge = true;
+//console.log(fullAge);
+
+
+
+// Lecture: variables 2
 var name = 'John';
-console.log(name);
-var lastName = 'Smith';
-console.log(lastName);
-var age = 26
-console.log(age);
-var fullAge = true;
-console.log(fullAge);
+var age = 26;
+console.log(name + ' ' + age);
+console.log(age + age);
+var job, isMarried;
+console.log(job);
