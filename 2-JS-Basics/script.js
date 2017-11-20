084018ab80079270f1583aa855a77a833424bfc9
YW <wangyouhao@YWde-MBP.hsd1.nj.comcast.net>
2017-11-20 15:53:26 -0500

    tried variables

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -17,7 +17,12 @@
 // Lecture: variables 2
 var name = 'John';
 var age = 26;
-console.log(name + ' ' + age);
-console.log(age + age);
+//console.log(name + ' ' + age);
+//console.log(age + age);
+
 var job, isMarried;
-console.log(job);
+//console.log(job);
+
+job = 'teacher';
+isMarried = false;
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
