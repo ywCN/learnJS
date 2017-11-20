fd2603a95bef51aab9e4f7f08c6f97917ad1b7f3
YW <wangyouhao@YWde-MBP.hsd1.nj.comcast.net>
2017-11-20 16:00:43 -0500

    tried prompt and alert

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -26,3 +26,12 @@ var job, isMarried;
 job = 'teacher';
 isMarried = false;
 console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = 'thirty six';
+job = 'driver'
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+
+//var lastName = prompt('What is the last name?');
+//console.log(lastName);
+
+alert(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
