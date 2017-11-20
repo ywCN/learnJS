b86af2de0c6a0e350ef91c7119a365587cfbffa9
ywCN <wangyouhao90@gmail.com>
2017-11-20 16:17:10 -0500

    tried operators

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -15,23 +15,47 @@
 
 
 // Lecture: variables 2
-var name = 'John';
-var age = 26;
-//console.log(name + ' ' + age);
-//console.log(age + age);
+//var name = 'John';
+//var age = 26;
+////console.log(name + ' ' + age);
+////console.log(age + age);
+//
+//var job, isMarried;
+////console.log(job);
+//
+//job = 'teacher';
+//isMarried = false;
+//console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+//
+//age = 'thirty six';
+//job = 'driver'
+//console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+//
+////var lastName = prompt('What is the last name?');
+////console.log(lastName);
+//
+//alert(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
 
-var job, isMarried;
-//console.log(job);
 
-job = 'teacher';
-isMarried = false;
-console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+// Lecture: operators
+var now = 2016;
+var birthYear = now - 26;
+birthYear = now - 26*2; // operator precedence
 
-age = 'thirty six';
-job = 'driver'
-console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
 
-//var lastName = prompt('What is the last name?');
-//console.log(lastName);
+console.log(birthYear);
 
-alert(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+var ageJohn = 30;
+var ageMark = 30;
+ageJohn = ageMark = (3 + 5) * 4 - 6; // = = operate from right to left (equal case)
+
+ageJohn++;
+ageMark *= 2;
+
+console.log(ageJohn + ' ' + ageMark);
+
+
+
+
+
+
