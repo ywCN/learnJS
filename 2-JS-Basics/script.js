24783e22571a98e672fbb2b700d419239859c3d9
YW <wangyouhao@YWde-MBP.hsd1.nj.comcast.net>
2017-11-20 15:34:53 -0500

    tried variables

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+//console.log('Hello World!');
+var name = 'John';
+console.log(name);
+var lastName = 'Smith';
+console.log(lastName);
+var age = 26
+console.log(age);
+var fullAge = true;
+console.log(fullAge);
