560f86a69d76f24153e0dbd9e207e382420f7942
ywCN <wangyouhao90@gmail.com>
2017-11-23 14:20:01 -0500

    tried problem

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -123,14 +123,27 @@ var ageMike = 29;
 var scoreJohn = heightJohn + 5 * ageJohn;
 var scoreMike = heightMike + 5 * ageMike;
 
-if (scoreJohn > scoreMike) {
-    console.log('John wins with ' + scoreJohn + ' points.');
-} else if (scoreJohn < scoreMike) {
-    console.log('Mike wins with ' + scoreMike + ' points.');
-} else {
-    console.log('There is a draw.');
-}
+//if (scoreJohn > scoreMike) {
+//    console.log('John wins with ' + scoreJohn + ' points.');
+//} else if (scoreJohn < scoreMike) {
+//    console.log('Mike wins with ' + scoreMike + ' points.');
+//} else { // scoreJohn === scoreMike
+//    console.log('There is a draw.');
+//}
 
+var heightMary = 158;
+var ageMary = 31;
+var scoreMary = heightMary + 5 * ageMary;
+
+if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
+    console.log('John wins with ' + scoreJohn + ' points.');
+} else if (scoreMike > scoreJohn && scoreMike > scoreMary) {
+    console.log('Mike wins with ' + scoreMike + ' points.');
+} else if (scoreMary > scoreJohn && scoreMary > scoreJohn) {
+    console.log('Mary wins with ' + scoreMary + ' points.');
+} else {
+    console.log('It\'s a draw.');
+}
 
 
 
