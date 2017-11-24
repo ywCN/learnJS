07459a2af07d52c6cc1f57d4f064a29519c3b760
ywCN <wangyouhao90@gmail.com>
2017-11-24 00:19:53 -0500

    tried for loops

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -302,8 +302,17 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // Lecture: Loops
 
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
 
-
+var names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+for (var i = names.length - 1; i >= 0; i--) {
+    console.log(names[i]);
+}
 
 
 
