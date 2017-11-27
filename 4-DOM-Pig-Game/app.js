4ae0ea0d33aac5677a353066e1e7f867283ad25f
ywCN <wangyouhao90@gmail.com>
2017-11-26 19:42:13 -0500

    tried random, floor, select, and display text

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -8,3 +8,31 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector('#score-0').textContent = dice; // id="name-0"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
