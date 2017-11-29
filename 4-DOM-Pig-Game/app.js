4fde65692f041e715c080bf51087b0c4b82c2b87
ywCN <wangyouhao90@gmail.com>
2017-11-28 22:28:58 -0500

    debugged

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -79,8 +79,8 @@ function init() {
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
-    document.getElementById('name-1').textContent = 'Player 1';
-    document.getElementById('name-2').textContent = 'Player 2';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
 }
 
 ///* to set or get text content of an element */
