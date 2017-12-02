63926a9f4872d019fed753b76a887c5402a10f52
wangyouhao <wangyouhao90@gmail.com>
2017-12-02 17:28:17 -0500

    tried prototype

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -18,6 +18,8 @@ Person.prototype.calculateAge = function() {
     console.log(2017 - this.yearOfBirth);
 };
 
+Person.prototype.lastName = 'Smith'; // all objects will inherit this property
+
 var john = new Person('John', 1990, 'teacher');
 var jane = new Person('Jane', 1969, 'designer');
 var mark = new Person('Mark', 1948, 'retired');
@@ -25,3 +27,5 @@ var mark = new Person('Mark', 1948, 'retired');
 john.calculateAge();
 jane.calculateAge();
 mark.calculateAge();
+
+console.log(john.lastName, jane.lastName, mark.lastName); 
