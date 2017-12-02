ddbb01962bcd0eaea5782e6902d3194f1c77b123
wangyouhao <wangyouhao90@gmail.com>
2017-12-02 17:25:54 -0500

    tried prototype

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -10,7 +10,18 @@ var Person = function (name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
+
 };
 
+// add a method in the prototype of Person, so object can still access this method even it is not in the constructor
+Person.prototype.calculateAge = function() {
+    console.log(2017 - this.yearOfBirth);
+};
 
 var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
