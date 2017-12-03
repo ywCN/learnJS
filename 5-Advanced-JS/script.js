da3e11a9729c5ad55fb8dbf2c89582b010d7fe1c
wangyouhao <wangyouhao90@gmail.com>
2017-12-02 23:24:12 -0500

    tried function return function

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -151,6 +151,32 @@ console.log(ages, fullAge, rates);
 */
 
 // Lecture: Functions returning functions
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log(name + ', what subject do you teach?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ' , what do you do?');
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('John');
+designerQuestion('John');
+
+
+// a different way
+interviewQuestion('teacher')('Mark'); // since it returns a function, so just call it like regular function()
+
 
 
 
