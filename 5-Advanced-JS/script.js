1e0b9edd30bd8d4a82e57742c97ecad4032428ad
wangyouhao <wangyouhao90@gmail.com>
2017-12-03 17:23:33 -0500

    added note for closures

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -211,7 +211,7 @@ game();
 
 ///////////////////////////////////////////////////////////
 // Lecture: Closures
-
+// An inner function has always access to the variables and  parameters of its outer function, even after the outer function has returned.
 function retirement(retirementAge) {
     var a = ' years left until retirement.';
     return function(yearOfBirth) {
@@ -223,6 +223,7 @@ function retirement(retirementAge) {
 var retirementUS = retirement(66);
 retirementUS(1990);
 
+retirement(66)(1990);
 
 
 
