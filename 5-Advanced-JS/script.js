7cefd4419c84ccd11248d5c23456c77b793c21d4
wangyouhao <wangyouhao90@gmail.com>
2017-12-02 21:13:18 -0500

    tried Object.create

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -51,12 +51,29 @@ true
 */
 
 // Object.create
+// This is not the most popular way of creating objects, but it allows use to implenment really complex inheritance structures. It allows us to specify which object should be a prototype.
+/*
+var personProto = {
+    calculateAge: function() {
+        console.log(2017 - this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto, 
+{
+    name: {value: 'Jane'},
+    yearOfBirth: {value: 1969},
+    job: {value: 'designer'}
+});
+*/
 
 
-
-
-
-
+// 
 
 
 
