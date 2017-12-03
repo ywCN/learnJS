d91378bea7e145411da4b49076759f9c9b8f1c2f
wangyouhao <wangyouhao90@gmail.com>
2017-12-03 17:20:16 -0500

    tried closures

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -212,8 +212,16 @@ game();
 ///////////////////////////////////////////////////////////
 // Lecture: Closures
 
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2017 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
 
-
+var retirementUS = retirement(66);
+retirementUS(1990);
 
 
 
