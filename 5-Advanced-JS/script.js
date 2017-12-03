191d456f1bd39661bb6ae25c9c6c3a49c1a152dc
wangyouhao <wangyouhao90@gmail.com>
2017-12-02 21:30:49 -0500

    tried primitives, objects, and functions

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -75,6 +75,37 @@ var jane = Object.create(personProto,
 
 // Primitives vs. Objects
 
+//Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a, b);
+
+//Objects
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+var obj2 = obj1;
+obj2.age = 30;
+console.log(obj1, obj2);
+
+//Functions
+var age = 27;
+var obj = {
+    name: 'John',
+    city: 'Lisbon'
+};
+
+function change(a, b) {
+    a = 30;
+    b.city = 'San Francisco';
+}
+
+change(age, obj);
+
+console.log(age); // 27
+console.log(city); // San Francisco
 
 
 
