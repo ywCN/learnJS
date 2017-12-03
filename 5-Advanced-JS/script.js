7419a6b0a31a3e28c0966998aeaf5dc0e7ae5f7a
wangyouhao <wangyouhao90@gmail.com>
2017-12-03 17:37:16 -0500

    tried closures

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -212,8 +212,9 @@ game();
 ///////////////////////////////////////////////////////////
 // Lecture: Closures
 // An inner function has always access to the variables and  parameters of its outer function, even after the outer function has returned.
+// Unlike stack, scope chain is always intact
 function retirement(retirementAge) {
-    var a = ' years left until retirement.';
+    var a = ' years left until retirement.';// still available after return because a is in scope chain
     return function(yearOfBirth) {
         var age = 2017 - yearOfBirth;
         console.log((retirementAge - age) + a);
@@ -221,9 +222,15 @@ function retirement(retirementAge) {
 }
 
 var retirementUS = retirement(66);
-retirementUS(1990);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
 
-retirement(66)(1990);
+retirementUS(1990);
+retirementGermany(1990);
+retirementIceland(1990);
+
+//retirement(66)(1990);
+
 
 
 
