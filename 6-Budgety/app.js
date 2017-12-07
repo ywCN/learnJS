721d0478c2eb719ccd631a60af6534701020b2a3
ywCN <wangyouhao90@gmail.com>
2017-12-06 21:09:47 -0500

    added eventListener for button

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -29,7 +29,9 @@ var UIController = (function() {
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-    
+    document.querySelector('.add__btn').addEventListener('click', function() {
+        console.log('clicked');
+    });
     
     
     
