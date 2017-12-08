1ba28adf20f752732bb63fd97955b841fd39e32e
ywCN <wangyouhao90@gmail.com>
2017-12-08 00:00:26 -0500

    displayed updated percentages

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -147,7 +147,8 @@ var UIController = (function() {
         incomeLabel: '.budget__income--value',
         expenseLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage'
     };
     
     return {
@@ -213,6 +214,26 @@ var UIController = (function() {
             
         },
         
+        displayPercentages: function(percentages) {
+            
+            var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+            
+            var nodeListForEach = function(list, callback) {
+                for (var i = 0; i < list.length; i++) {
+                    callback(list[i], i);
+                }
+            };
+            
+            nodeListForEach(fields, function(current, index) {
+                if (percentages[index] > 0) {
+                    current.textContent = percentages[index] + '%';
+                } else {
+                    current.textContent = '---';
+                }
+            });
+            
+        },
+        
         getDOMstrings: function() { // to make DOMstrings public
             return DOMstrings;
         }
@@ -257,7 +278,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         var percentages = budgetCtrl.getPercentages();
         
         // 3. update the UI with the new percentages
-        console.log(percentages);
+        UICtrl.displayPercentages(percentages);
     };
     
     var ctrlAddItem = function() {
