16f61a52b9c300c296ab0fbac85c2485a4d6740d
ywCN <wangyouhao90@gmail.com>
2017-12-08 18:15:30 -0500

    tried more arrow functions for this keyword

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -140,6 +140,65 @@ console.log(ages6);
 //////////////////////////////////////////////////////////////////////
 // Lecture: Arrow Functions 2
 
+// ES5
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+        var self = this; // if just use this below, it will be undefined
+        document.querySelector('.green').addEventListener('click', function() {
+            var str = 'This is box number ' + self.position + ' and it is ' + self.color;
+            alert(str);
+        });
+    }
+};
+//box5.clickMe();
+
+
+// ES6
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+        document.querySelector('.green').addEventListener('click', () => {
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+};
+//box6.clickMe();
+
+
+
+const box66 = {
+    color: 'green',
+    position: 1,
+    clickMe: () => {
+        document.querySelector('.green').addEventListener('click', () => {
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+};
+//box66.clickMe(); // this is now pointing to the window object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
