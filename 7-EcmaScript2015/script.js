5523604f9c867c49f9e5b6dafe238d75fc497342
ywCN <wangyouhao90@gmail.com>
2017-12-08 17:03:33 -0500

    tried new way to create string

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -56,6 +56,34 @@ console.log(i); // 12
 // Lecture: Blocks and IIFEs
 
 
+// ES5
+(function() {
+    var c = 3;
+})();
+
+
+// ES6
+{
+    let b = 3;
+}
+
+
+
+//////////////////////////////////////////////////////////////////////
+// Lecture: Strings
+
+let firstName = 'john';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+function calcAge(year) {
+    return 2016 - year;
+}
+
+// ES5
+console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today, he is ' + calcAge(yearOfBirth) + ' years old.');
+
+// ES6
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
 
 
 
