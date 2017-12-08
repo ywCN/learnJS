d17cdd26d9eff555edd8aaf44023447a02bda82a
ywCN <wangyouhao90@gmail.com>
2017-12-07 23:06:38 -0500

    deleted item from data structure

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -78,6 +78,19 @@ var budgetController = (function() {
             
         },
         
+        deleteItem: function(type, id) {
+            var ids, index;
+            ids = data.allItems[type].map(function(current) {
+                return current.id;
+            });
+            
+            index = ids.indexOf(id);
+            
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+        },
+        
         getBudget: function() {
             return {
                 budget: data.budget,
@@ -232,10 +245,10 @@ var controller = (function(budgetCtrl, UICtrl) {
             // inc-1
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
             
             // 1. delete the item from the data structure
-            
+            budgetCtrl.deleteItem(type, ID);
             
             // 2. delete the item from the UI
             
